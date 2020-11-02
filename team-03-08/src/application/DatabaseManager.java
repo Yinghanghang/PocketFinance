5c0b0da3f8d1807d0fd2cd94dae652b709a556cf
yingying <yingying@yingyings-mbp.lan>
2020-11-01 22:02:19 -0800

    Add DatabaseManager and Transaction classes.

---
+++ team-03-08/src/application/DatabaseManager.java
@@ -0,0 +1,100 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseManager {
+	private Connection conn;
+
+	/**
+	 * Creates a connection to database.
+	 */
+	private DatabaseManager(String database, String user, String password) throws SQLException {
+		this.conn = DriverManager.getConnection(database, user, password);	
+	}
+
+	/**
+	 * Creates a database manager.
+	 */
+	public static DatabaseManager create(String database, String user, String password) throws ClassNotFoundException, SQLException { 
+		Class.forName("com.mysql.jdbc.Driver");
+		return new DatabaseManager(database, user, password);
+	}
+
+	/**
+	 * Gets transactions from database
+	 */
+	public List<Transaction> getTransactions() throws SQLException {
+		List<Transaction> result = new ArrayList<>();
+		Statement statement = conn.createStatement();
+		try (ResultSet resultSet = statement.executeQuery("select Date, Amount, Category, Type from transaction")) {
+			while (resultSet.next()) {
+				result.add(new Transaction(resultSet.getDate(1), resultSet.getDouble(2), resultSet.getString(3), resultSet.getString(4)));
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Get the sum of income from 2020/1/1 to present
+	 */
+	public double getIncome(LocalDate from, LocalDate to) throws SQLException {
+		try (PreparedStatement statement = conn.prepareStatement("select sum(Amount) from transaction where date between ? and ? and type=\"Income\"")) {
+			statement.setDate(1, Date.valueOf(from));
+			statement.setDate(2, Date.valueOf(to));
+			try (ResultSet resultSet = statement.executeQuery()) {
+				if (resultSet.next()) {
+					return resultSet.getDouble(1);					
+				}
+				return 0;
+			}
+		}
+	}
+
+	/**
+	 * Get the sum of expense from 2020/1/1 to present
+	 */
+	public double getExpense(LocalDate from, LocalDate to) throws SQLException {
+		try (PreparedStatement statement = conn.prepareStatement("select sum(Amount) from transaction where date between ? and ? and type=\"Expense\"")) {
+			statement.setDate(1, Date.valueOf(from));
+			statement.setDate(2, Date.valueOf(to));
+			try (ResultSet resultSet = statement.executeQuery()) {
+				if (resultSet.next()) {
+					return resultSet.getDouble(1);					
+				}
+				return 0;
+			}
+		}
+	}
+
+
+	/**
+	 * Get the sum of net income from 2020/1/1 to present
+	 */
+	public double getNetIncome(LocalDate from, LocalDate to) throws SQLException {
+		return getIncome(from, to) - getExpense(from, to);
+	}
+	
+	public boolean addTransaction(Transaction transaction) throws SQLException {
+		
+		try (PreparedStatement statement = conn.prepareStatement("INSERT INTO transaction(Date, Type, Category, Amount) VALUES (?, ?, ?, ?)")) {
+			statement.setDate(1, transaction.date());
+			statement.setString(2, transaction.type());
+			statement.setString(3, transaction.category());
+			statement.setDouble(4, transaction.amount());
+			return statement.executeUpdate() != 0;
+		}
+	}
+
+	public void close() throws SQLException {
+		conn.close();
+	}
+}
---
+++ team-03-08/src/application/Transaction.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.sql.Date;
+
+public class Transaction {
+	private final double amount;
+	private final Date date;
+	private final String category;
+	private final String type;
+
+	Transaction(Date date, double amount, String category, String type) {
+		this.date = date;
+		this.amount = amount;
+		this.category = category;
+		this.type = type;
+	}
+
+	public double amount() { return amount; }
+
+	public Date date() { return date; }
+
+	public String category() { return category; }
+	
+	public String type() { return type; }
+}
